bed6142f043f51d94f9400cea195145648cb4c14
dltt83 <dltt83@durham.ac.uk>
2022-08-10 18:10:06 +0100

    added background images to divs, added buttons to view page (links not yet working

--- client/index.js
+++ client/index.js
@@ -1,9 +1,7 @@
 // function fro handling page on load
 window.addEventListener('load', async function (event) {
-    let body = ["something", "something else"]
-
     let response = await fetch('http://127.0.0.1:8090/projects');
-    body = await response.json();
+    let body = await response.json();
 
     // reder page content for home page
     renderContent('Home', body);
@@ -18,19 +16,38 @@ function renderContent (page, projects) {
         let container = document.getElementById('main_page');
         container.innerHTML = "";
 
-        for (let thing of projects) {
+        for (let currentProject of projects) {
+            // create div and assign class
             let div = document.createElement('div');
             div.setAttribute("class", "portItem");
-            div.style.backgroundImage="url(./testImage.png)"
-            div.style.backgroundPosition="100% 70%"
-            // div.setAttribute("id", thing);
-
-            let heading = document.createElement('h1');
-            heading.innerHTML = thing;
-
-            div.innerHTML = heading.innerHTML;
             
+            // set div background image and position
+            div.style.backgroundImage=`url(media/${currentProject}-cover.png)`
+            div.style.backgroundPosition="100% 70%"
+
+            // create view button in div and give id and class
+            const viewButton = document.createElement('button');
+            viewButton.innerHTML = currentProject
+            viewButton.setAttribute('id', `${currentProject}-view`);
+            viewButton.setAttribute('class', 'portItem-button')
+            
+            // create function on button press
+            viewButton.addEventListener('click', async function (event) {
+                event.preventDefault();
+                
+                renderContent(currentProject, projects);
+            });
+            
+            
+            // append children to div
+            div.appendChild(viewButton);
+            
+            // append div to main content div
             container.appendChild(div);
         }
+    } else if (page === "etsAutopilot") {
+        console.log("ets autopilot page")
+    } else if (page === "bedBooking") {
+        console.log("bed booking page")
     }
 }
