1fa15b8b9d43952d2748e8dbc0cae92273052bf9
dltt83 <dltt83@durham.ac.uk>
2022-08-18 20:49:06 +0100

    Added about and links pages as well as imporved compatibility for new projects

--- client/index.js
+++ client/index.js
@@ -14,6 +14,7 @@ function renderNavbar (SITE_DATA) {
     let navbarDiv = document.getElementById("navbar-div");
     navbarDiv.innerHTML = ""
     let buttons = ["Josh Lodge Software", "About Me", "Contact Info"];
+    let links = ["Home", "About", "Links"]
 
     for (let currentButton of buttons) {
         // create new button and give class
@@ -26,24 +27,16 @@ function renderNavbar (SITE_DATA) {
             homeHeading.setAttribute("class", "navbar-heading");
             homeHeading.innerHTML = currentButton;
             newButton.appendChild(homeHeading)
-
-            // create function on button press to load main page
-            newButton.addEventListener('click', function (event) {
-                event.preventDefault();
-                
-                renderContent("Home", SITE_DATA)
-            });
         } else {
             newButton.innerHTML = currentButton;
-            
-            // create function on button press to load relevant page
-            newButton.addEventListener('click', function (event) {
-                event.preventDefault();
-                
-                console.log(currentButton)
-            });
         }
 
+        // create function on button press to load main page
+        newButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            
+            renderContent(links[buttons.indexOf(currentButton)], SITE_DATA)
+        });
 
         navbarDiv.appendChild(newButton)
     }
@@ -69,7 +62,7 @@ function renderHome (SITE_DATA) {
         // create view button in div and give id and class
         const viewButton = document.createElement('button');
         viewButton.innerHTML = currentProject.prettyName
-        viewButton.setAttribute('id', `${currentProject.id}-view`);
+        viewButton.setAttribute('id', `${currentProject.id}-button`);
         viewButton.setAttribute('class', 'portItem-button')
         
         // create function on button press
@@ -105,11 +98,13 @@ function renderProject (project) {
         container.appendChild(newPara)
     }
 
-    let githubLink = document.createElement("a")
-    githubLink.setAttribute("href", project.github)
-    githubLink.setAttribute("class", "gitLink")
-    githubLink.innerHTML = "Github Repository"
-    container.appendChild(githubLink)
+    if (project.github != "") {
+        let githubLink = document.createElement("a")
+        githubLink.setAttribute("href", project.github)
+        githubLink.setAttribute("class", "link")
+        githubLink.innerHTML = "Github Repository"
+        container.appendChild(githubLink)
+    }
 
     // loop through images in json and add to page
     for (let imageName of project.otherImages) {
@@ -117,7 +112,83 @@ function renderProject (project) {
         newImage.setAttribute("src", `./media/${imageName}`)
         newImage.setAttribute("class", "project-image")
         container.appendChild(newImage)
+
+        caption = project.captions[project.otherImages.indexOf(imageName)]
+        if (caption != "") {
+            let imageCaption = document.createElement("p")
+            imageCaption.innerHTML = caption
+            container.appendChild(imageCaption)
+        }
     }
+}
+
+function renderAbout (SITE_DATA) {
+    // get main contatiner and clear
+    let container = document.getElementById('main_page');
+    container.innerHTML = "";
+
+    // give page title
+    let title = document.createElement("h1")
+    title.setAttribute("class", "page-title")
+    title.innerHTML = "About Me"
+    container.appendChild(title)
+
+    // loop through paragraphs from json and add to page
+    for (let paragraph of SITE_DATA.about.waffle) {
+        let newPara = document.createElement("p")
+        newPara.innerHTML = paragraph
+        container.appendChild(newPara)
+    }
+
+    let cvButton = document.createElement("a")
+    cvButton.setAttribute("href", SITE_DATA.links.cv)
+    cvButton.setAttribute("class", "link")
+    cvButton.innerHTML = "Personal CV"
+    container.appendChild(cvButton) 
+
+    // loop through images in json and add to page
+    for (let imageName of SITE_DATA.about.pictures) {
+        let newImage = document.createElement("img")
+        newImage.setAttribute("src", `./media/${imageName}`)
+        newImage.setAttribute("class", "about-image")
+        container.appendChild(newImage)
+    }
+}
+
+function renderContact (SITE_DATA) {
+    // get main contatiner and clear
+    let container = document.getElementById('main_page');
+    container.innerHTML = "";
+
+    // give page title
+    let title = document.createElement("h1")
+    title.setAttribute("class", "page-title")
+    title.innerHTML = "Contact Me"
+    container.appendChild(title)
+
+    let email = document.createElement("a")
+    email.setAttribute("href", `mailto:${SITE_DATA.links.email}`)
+    email.setAttribute("class", "link")
+    email.innerHTML = "Email: josh.lodge07@gmail.com"
+    container.appendChild(email)
+
+    let githubLink = document.createElement("a")
+    githubLink.setAttribute("href", SITE_DATA.links.github)
+    githubLink.setAttribute("class", "link")
+    githubLink.innerHTML = "Personal Github Account"
+    container.appendChild(githubLink)
+
+    let linkedIn = document.createElement("a")
+    linkedIn.setAttribute("href", SITE_DATA.links.linkedIn)
+    linkedIn.setAttribute("class", "link")
+    linkedIn.innerHTML = "LinkedIn"
+    container.appendChild(linkedIn)
+
+    let cvButton = document.createElement("a")
+    cvButton.setAttribute("href", SITE_DATA.links.cv)
+    cvButton.setAttribute("class", "link")
+    cvButton.innerHTML = "Personal CV"
+    container.appendChild(cvButton) 
 }
 
 // funciton to render main content on DOM
@@ -130,6 +201,10 @@ function renderContent (page, SITE_DATA) {
         // call function to render home page
         renderHome(SITE_DATA);
 
+    } else if (page === 'About') {
+        renderAbout(SITE_DATA)
+    } else if (page === 'Links') {
+        renderContact(SITE_DATA)
     } else {
         // call function to render page with corrent content
         // renderProject(SITE_DATA.projects[id=page]);      <- should work, dont know why it doesnt
