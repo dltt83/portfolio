4c8032fb99ba562a936b23a3fe59ce0a2f2a82c3
dltt83 <dltt83@durham.ac.uk>
2022-08-29 15:51:48 +0100

    Adding browser back functionality

--- client/index.js
+++ client/index.js
@@ -10,13 +10,23 @@ window.addEventListener('load', async function (event) {
     renderContent('Home', SITE_DATA);
 });
 
-window.addEventListener('popstate', function(event) {
-});
-
-onpopstate = (event) => {
-    console.log("back button pressed");
-    renderContent('Home', SITE_DATA);
-};
+function HandleBackFunctionality() {  
+    if(window.event) {  
+        if(window.event.clientX < 40 && window.event.clientY < 0) {  
+            alert("Browser back button is clicked...");  
+        } else {  
+            alert("Browser refresh button is clicked...");  
+        }  
+    } else {  
+        if(event.currentTarget.performance.navigation.type == 1) {  
+            alert("Browser refresh button is clicked...");
+        }
+        
+        if(event.currentTarget.performance.navigation.type == 2) {  
+            alert("Browser back button is clicked...");  
+        }  
+    }
+} 
 
 // ===== funcitons for rendering content to DOM =====
 
